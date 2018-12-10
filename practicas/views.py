cce5d0d33ec8453308906b7e6194399838d65026
nifled <erickdelfinmolina@gmail.com>
2018-12-10 04:24:24 -0700

    Initial commit7

--- practicas/views.py
+++ practicas/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Materia, Ciclo
 
-# Create your views here.
+
+def ciclos(request):
+  ciclos = Ciclo.objects.all().order_by('-nombre')
+
+  context = {'ciclos': ciclos}
+
+  return render(request, 'practicas/ciclo_list.html', context)
