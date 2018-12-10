668f83eea3d7d09089e09ce7441fb2e25a75b354
nifled <erickdelfinmolina@gmail.com>
2018-12-10 03:58:16 -0700

    Initial commit4

--- practicas/models.py
+++ practicas/models.py
@@ -1,23 +1,41 @@
 from django.db import models
 
 
+class Ciclo(models.Model):
+  nombre = models.CharField(max_length=55)
+
+  def __str__(self):
+    return self.nombre
+
+
+class Materia(models.Model):
+  nombre = models.CharField(max_length=255)
+  maestro = models.CharField(max_length=255)
+  ciclo = models.ForeignKey(Ciclo, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.nombre
+
+
 class Practica(models.Model):
   numero_practica = models.PositiveIntegerField()
   titulo = models.CharField(max_length=255)
   descripcion = models.CharField(max_length=255)
   archivo = models.FileField(upload_to='practicas/')
   fecha = models.DateField(auto_now=True)
+  materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
 
   class Meta:
     verbose_name = 'Practica'
     verbose_name_plural = 'Practicas'
 
   def __str__(self):
-    return self.titulo
+    return f'{self.titulo} - {self.materia.nombre}'
 
 
 class Alumno(models.Model):
   nombre = models.CharField(max_length=85)
+  materia = models.ForeignKey(Materia, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
