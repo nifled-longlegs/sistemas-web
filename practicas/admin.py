00ce037c3841620a139d137365725fb1dc96cd38
nifled <erickdelfinmolina@gmail.com>
2018-12-10 04:02:34 -0700

    Initial commit5

--- practicas/admin.py
+++ practicas/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Practica, Alumno, PracticaAlumno
+from .models import Practica, Alumno, PracticaAlumno, Ciclo, Materia
 
 # Register your models here.
+admin.site.register(Ciclo)
+admin.site.register(Materia)
 admin.site.register(Practica)
 admin.site.register(Alumno)
 admin.site.register(PracticaAlumno)
